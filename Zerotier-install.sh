64395955348864f24ed2b79b4a2f42d7b62d615e
gegemon2012 <suprunmv@gmail.com>
2026-03-05 13:50:38 +0400

    Update Zerotier-install.sh

--- Zerotier-install.sh
+++ Zerotier-install.sh
@@ -1,73 +1,56 @@
 #!/bin/sh
 
-# 1. Обновление списков и установка
+# 1. Установка
 opkg update
 opkg install zerotier
 
-# 2. Настройка базового конфига ZeroTier
-ZT_CONF="/etc/config/zerotier"
-
+# 2. Настройка через UCI (фиксим Invalid argument)
 echo "Введите ваш ZeroTier Network ID:"
 read ZT_ID
 
-# Включаем сервис и задаем ID через UCI (это надежнее, чем awk/sed)
-uci set zerotier.sample_config.enabled='1'
-uci set zerotier.sample_config.join='$ZT_ID'
-# Если в вашем конфиге используется другая секция вместо sample_config, 
-# скрипт ниже создаст стандартную структуру:
-if [ $? -ne 0 ]; then
-    uci set zerotier.openwrt_network=zerotier
-    uci set zerotier.openwrt_network.enabled='1'
-    uci set zerotier.openwrt_network.id='$ZT_ID'
-fi
+# Удаляем старые записи, если они были, чтобы не плодить ошибки
+uci delete zerotier.openwrt_network 2>/dev/null
+
+# Создаем чистую конфигурацию
+uci set zerotier.openwrt_network=zerotier
+uci set zerotier.openwrt_network.enabled='1'
+uci set zerotier.openwrt_network.id="$ZT_ID"
 uci commit zerotier
 
-# 3. Создание сетевого интерфейса в OpenWrt
-# Это то, чего не хватало: привязываем виртуальное устройство zt+ к системе
+# 3. Настройка сети и Firewall
 uci set network.zerotier=interface
 uci set network.zerotier.proto='none'
 uci set network.zerotier.device='zt+'
 uci commit network
 
-# 4. Настройка Firewall (Зоны и правила)
-# Удаляем старую зону, если она была, чтобы избежать дублей
-uci delete firewall.zerotier_zone 2>/dev/null
-uci set firewall.zerotier_zone=zone
-uci set firewall.zerotier_zone.name='zerotier'
-uci set firewall.zerotier_zone.input='ACCEPT'
-uci set firewall.zerotier_zone.forward='ACCEPT'
-uci set firewall.zerotier_zone.output='ACCEPT'
-uci set firewall.zerotier_zone.network='zerotier'
-uci set firewall.zerotier_zone.masq='1'
-uci set firewall.zerotier_zone.mtu_fix='1'
-
-# Разрешаем пересылку трафика между LAN и ZeroTier
-uci add firewall forwarding
-uci set firewall.@forwarding[-1].src='lan'
-uci set firewall.@forwarding[-1].dest='zerotier'
-
-uci add firewall forwarding
-uci set firewall.@forwarding[-1].src='zerotier'
-uci set firewall.@forwarding[-1].dest='lan'
-
-# Разрешаем входящий трафик для самого ZeroTier (UDP 9993) на WAN
-uci add firewall rule
-uci set firewall.@rule[-1].name='Allow-ZeroTier-UDP'
-uci set firewall.@rule[-1].src='wan'
-uci set firewall.@rule[-1].dest_port='9993'
-uci set firewall.@rule[-1].proto='udp'
-uci set firewall.@rule[-1].target='ACCEPT'
-
+# Настройка зоны (используем понятное имя)
+uci delete firewall.zt_zone 2>/dev/null
+uci set firewall.zt_zone=zone
+uci set firewall.zt_zone.name='zerotier'
+uci set firewall.zt_zone.input='ACCEPT'
+uci set firewall.zt_zone.forward='ACCEPT'
+uci set firewall.zt_zone.output='ACCEPT'
+uci set firewall.zt_zone.network='zerotier'
+uci set firewall.zt_zone.masq='1'
 uci commit firewall
 
-# 5. Перезапуск всех служб
-/etc/init.d/network restart
-/etc/init.d/firewall restart
+# 4. Запуск служб
 /etc/init.d/zerotier enable
 /etc/init.d/zerotier restart
+/etc/init.d/network restart
+/etc/init.d/firewall restart
+
+# Даем сервису 5 секунд, чтобы он "проснулся"
+echo "Ожидание запуска ZeroTier..."
+sleep 5
 
 echo "------------------------------------------------------"
 echo "Установка завершена!"
-echo "ВАЖНО: Зайдите в панель my.zerotier.com и АВТОРИЗУЙТЕ роутер."
-echo "Ваш Node ID: $(zerotier-cli status | awk '{print $3}')"
+# Пробуем получить ID снова
+NODE_ID=$(zerotier-cli status | awk '{print $3}')
+if [ -z "$NODE_ID" ]; then
+    echo "Ошибка: Сервис еще не запущен. Попробуйте выполнить 'zerotier-cli status' через минуту."
+else
+    echo "Ваш Node ID: $NODE_ID"
+fi
 echo "------------------------------------------------------"
