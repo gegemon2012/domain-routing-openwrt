c34116976da8a35694b2ea6dfeabe0f3c1c0dd44
gegemon2012 <suprunmv@gmail.com>
2026-03-14 19:41:35 +0400

    Delete lst to json converter.py

--- lst to json converter.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("./ipsum.lst") as input_file:
-    with open("./ipsum_lst.json", "w") as output_file:
-        json.dump(
-            [{"hostname": line.strip()} for line in input_file.readlines()],
-            output_file,
-            indent=2,
-        )
