90fc9cf528750a61ac33fea23666d6dac6fa389f
GIlGAmesh <suprunmv@gmail.com>
2026-03-29 12:05:12 +0400

    Update zapret-install.sh

--- zapret-install.sh
+++ zapret-install.sh
@@ -1,59 +1,99 @@
-install_zapret() {
-    ZAPRET_URL="https://github.com/bol-van/zapret/releases/download/v72.12/zapret-v72.12-openwrt-embedded.tar.gz"
-    TOP_FOLDER="zapret-v72.12"
+#!/bin/sh
 
-    wget "$ZAPRET_URL" -O /tmp/zapret.tar.gz
-    tar -xzf /tmp/zapret.tar.gz -C /tmp/
-    chmod +x /tmp/"$TOP_FOLDER"/*.sh
-    if /tmp/"$TOP_FOLDER"/install_easy.sh; then
-        cat > /opt/zapret/config << 'EOF'
-FWTYPE=nftables
+# automated script for easy uninstalling zapret
 
-SET_MAXELEM=522288
-IPSET_OPT="hashsize 262144 maxelem $SET_MAXELEM"
+EXEDIR="$(dirname "$0")"
+EXEDIR="$(cd "$EXEDIR"; pwd)"
+ZAPRET_BASE=${ZAPRET_BASE:-"$EXEDIR"}
+ZAPRET_RW=${ZAPRET_RW:-"$ZAPRET_BASE"}
+ZAPRET_CONFIG=${ZAPRET_CONFIG:-"$ZAPRET_RW/config"}
+ZAPRET_CONFIG_DEFAULT="$ZAPRET_BASE/config.default"
+IPSET_DIR="$ZAPRET_BASE/ipset"
 
-IP2NET_OPT4="--prefix-length=22-30 --v4-threshold=3/4"
-IP2NET_OPT6="--prefix-length=56-64 --v6-threshold=5"
-AUTOHOSTLIST_RETRANS_THRESHOLD=3
-AUTOHOSTLIST_FAIL_THRESHOLD=3
-AUTOHOSTLIST_FAIL_TIME=60
-AUTOHOSTLIST_DEBUGLOG=0
-
-MDIG_THREADS=30
-
-GZIP_LISTS=1
-
-DESYNC_MARK=0x40000000
-DESYNC_MARK_POSTNAT=0x20000000
-
-NFQWS_ENABLE=1
-NFQWS_PORTS_TCP=443,2053,2083,2087,2096,8443
-NFQWS_PORTS_UDP=1400,19294-19344,50000-50100
-NFQWS_TCP_PKT_OUT=$((6+$AUTOHOSTLIST_RETRANS_THRESHOLD))
-NFQWS_TCP_PKT_IN=3
-NFQWS_UDP_PKT_OUT=$((6+$AUTOHOSTLIST_RETRANS_THRESHOLD))
-NFQWS_UDP_PKT_IN=0
-NFQWS_OPT="
---filter-udp=1400 --filter-l7=stun --dpi-desync=fake --dpi-desync-repeats=6 --new
---filter-udp=19294-19344,50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new
---filter-tcp=2053,2083,2087,2096,8443 --hostlist-domains=discord.media --dpi-desync=multisplit --dpi-desync-split-pos=2,sniext+1 --dpi-desync-split-seqovl=679 --dpi-desync-split-seqovl-pattern="/opt/zapret/files/fake/tls_clienthello_www_google_com.bin" --new
---filter-tcp=443 --dpi-desync=multisplit --dpi-desync-repeats=2 --dpi-desync-split-pos=2,sniext+1 --dpi-desync-split-seqovl=679 --dpi-desync-split-seqovl-pattern="/opt/zapret/files/fake/tls_clienthello_www_google_com.bin" --hostlist=/tmp/zapret.lst
-"
-
-MODE_FILTER=none
-
-FLOWOFFLOAD=donttouch
-
-INIT_APPLY_FW=1
-
-DISABLE_IPV6=1
-
-FILTER_TTL_EXPIRED_ICMP=1
-EOF
-    else
-        echo "Error: install_easy.sh failed"
-        exit 1
-    fi
+[ -f "$ZAPRET_CONFIG" ] || {
+	ZAPRET_CONFIG_DIR="$(dirname "$ZAPRET_CONFIG")"
+	[ -d "$ZAPRET_CONFIG_DIR" ] || mkdir -p "$ZAPRET_CONFIG_DIR"
+	cp "$ZAPRET_CONFIG_DEFAULT" "$ZAPRET_CONFIG"
 }
 
-install_zapret
+. "$ZAPRET_CONFIG"
+. "$ZAPRET_BASE/common/base.sh"
+. "$ZAPRET_BASE/common/elevate.sh"
+. "$ZAPRET_BASE/common/fwtype.sh"
+. "$ZAPRET_BASE/common/dialog.sh"
+. "$ZAPRET_BASE/common/ipt.sh"
+. "$ZAPRET_BASE/common/nft.sh"
+. "$ZAPRET_BASE/common/installer.sh"
+
+remove_systemd()
+{
+	clear_ipset
+	service_stop_systemd
+	service_remove_systemd
+	timer_remove_systemd
+	nft_del_table
+	crontab_del
+}
+
+remove_openrc()
+{
+	clear_ipset
+	service_remove_openrc
+	nft_del_table
+	crontab_del
+}
+
+remove_linux()
+{
+	INIT_SCRIPT_SRC="$EXEDIR/init.d/sysv/zapret2"
+
+	clear_ipset
+
+	echo \* executing sysv init stop
+	"$INIT_SCRIPT_SRC" stop
+
+	nft_del_table
+	crontab_del
+
+	echo
+	echo '!!! WARNING. YOUR UNINSTALL IS INCOMPLETE !!!'
+	echo 'you must manually remove zapret auto start from your system'
+}
+
+remove_openwrt()
+{
+	OPENWRT_FW_INCLUDE=/etc/firewall.zapret2
+
+	clear_ipset
+	service_remove_sysv
+	remove_openwrt_firewall
+	remove_openwrt_iface_hook
+	nft_del_table
+	restart_openwrt_firewall
+	crontab_del
+	remove_extra_pkgs_openwrt
+	echo
+	echo to fully remove zapret : rm -r \"$ZAPRET_BASE\"
+}
+
+fix_sbin_path
+check_system
+require_root
+
+case $SYSTEM in
+	systemd)
+		remove_systemd
+		;;
+	openrc)
+		remove_openrc
+		;;
+	linux)
+		remove_linux
+		;;
+	openwrt)
+		remove_openwrt
+		;;
+esac
+
+
+exitp 0
