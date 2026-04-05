8d0afa2eb8e238377fc92d622269c5b243e32c6c
GI[l]GAmesh <suprunmv@gmail.com>
2026-04-05 12:57:03 +0400

    Update optimization-router-install.sh

--- optimization-router-install.sh
+++ optimization-router-install.sh
@@ -49,38 +49,55 @@ uci commit system
 # Перезапуск сервиса
 /etc/init.d/zram restart
 
-# 3. IPv6 Меню
+# 3. IPv6 Меню (с правкой ULA для устранения задержек)
 echo -e "\n--- Шаг 3: IPv6 ---"
-echo "1) Отключить (убрать ошибки в логах)"
-echo "2) Включить"
+echo "1) Полностью отключить (убрать ошибки в логах и задержки в браузере)"
+echo "2) Включить (стандартные настройки)"
 echo "3) Пропустить"
 printf "Выбор: "
 read choice
 
 case "$choice" in
     1)
+        echo "Отключение IPv6 и очистка ULA префикса..."
+        # Удаляем интерфейс wan6
         uci -q delete network.wan6
+        
+        # Отключаем анонсы и DHCPv6 на LAN
         uci set dhcp.lan.ra='disabled'
         uci set dhcp.lan.dhcpv6='disabled'
         uci set dhcp.lan.ra_management='0'
+        
+        # ВАЖНО: Очищаем ULA префикс, чтобы роутер не создавал адреса fd45:...
+        uci set network.globals.ula_prefix=''
+        
+        uci commit
         /etc/init.d/odhcpd stop
         /etc/init.d/odhcpd disable
-        uci commit
         /etc/init.d/network restart
-        echo "IPv6 отключен."
+        echo "✅ IPv6 полностью отключен, локальные адреса (ULA) удалены."
         ;;
     2)
+        echo "Включение IPv6..."
         uci set network.wan6=interface
         uci set network.wan6.proto='dhcpv6'
         uci set network.wan6.device='@wan'
+        
+        # Возвращаем стандартный префикс (если был пустой)
+        [ -z "$(uci get network.globals.ula_prefix 2>/dev/null)" ] && uci set network.globals.ula_prefix='fd00::/48'
+        
         uci set dhcp.lan.ra='server'
         uci set dhcp.lan.dhcpv6='server'
         uci set dhcp.lan.ra_management='1'
+        
+        uci commit
         /etc/init.d/odhcpd enable
         /etc/init.d/odhcpd start
-        uci commit
         /etc/init.d/network restart
-        echo "IPv6 включен."
+        echo "✅ IPv6 включен."
+        ;;
+    *)
+        echo "Пропущено."
         ;;
 esac
 
