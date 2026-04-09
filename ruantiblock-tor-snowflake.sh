1e0654a7260e5793c31624b4af40b5f436d7b104
GI[l]GAmesh <suprunmv@gmail.com>
2026-04-09 21:29:15 +0400

    Update ruantiblock-tor-snowflake.sh

--- ruantiblock-tor-snowflake.sh
+++ ruantiblock-tor-snowflake.sh
@@ -1,36 +1,18 @@
-#!/bin/sh
-
-# Установка необходимого модуля
-opkg update
+# 1. Установка клиента
 opkg install snowflake-client
 
-TORRC="/etc/tor/torrc"
-SNOWFLAKE_BIN=$(which snowflake-client)
+# 2. Настройка конфигурации Tor
+# Вставляем настройки Snowflake в конец /etc/tor/torrc
+# Мы используем sed, чтобы гарантированно удалить старые мосты перед добавлением
+sed -i '/Bridge /d' /etc/tor/torrc
+sed -i '/ClientTransportPlugin/d' /etc/tor/torrc
+sed -i '/UseBridges/d' /etc/tor/torrc
 
-if [ -z "$SNOWFLAKE_BIN" ]; then
-    echo "Ошибка: snowflake-client не установлен!"
-    exit 1
-fi
-
-# Делаем копию конфига, как в основном скрипте ruantiblock
-cp $TORRC "${TORRC}.bak.$(date +%s)"
-
-# Очищаем старые настройки мостов и транспортов, чтобы не дублировать
-sed -i '/UseBridges/d' $TORRC
-sed -i '/ClientTransportPlugin snowflake/d' $TORRC
-sed -i '/Bridge snowflake/d' $TORRC
-
-# Добавляем конфигурацию Snowflake
-# Мы используем публичные STUN-серверы и брокер Tor Project
-cat <<EOF >> $TORRC
-
-# Автоматическая настройка Snowflake
+cat <<EOF >> /etc/tor/torrc
 UseBridges 1
-ClientTransportPlugin snowflake exec $SNOWFLAKE_BIN -url https://snowflake-broker.torproject.net.global.prod.fastly.net/ -front cdn.sstatic.net -ice stun:stun.l.google.com:19302,stun:stun.voip.blackberry.com:3478
+ClientTransportPlugin snowflake exec /usr/bin/snowflake-client -url https://snowflake-broker.torproject.net.global.prod.fastly.net/ -front cdn.sstatic.net -ice stun:stun.l.google.com:19302,stun:stun.voip.blackberry.com:3478
 Bridge snowflake 192.0.2.3:1 2B280B23E1107F24836209A3206656D32CC97D06
 EOF
 
-# Перезагружаем Tor
+# 3. Перезапуск
 /etc/init.d/tor restart
-
-echo "Snowflake успешно интегрирован в Tor."
