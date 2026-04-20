33f79b26b68446f2bff97e4df4067959aa910ddb
GI[l]GAmesh <suprunmv@gmail.com>
2026-04-20 19:30:04 +0400

    Update Recursive-DNS-install.sh

--- Recursive-DNS-install.sh
+++ Recursive-DNS-install.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 
-echo "=== RECURSIVE DNS & NTP FIX ==="
+echo "=== RECURSIVE DNS & NTP FIX (RU OPTIMIZED) ==="
 
-# 1. NTP Fix (Безопасно, всегда вносим правки в hosts)
-sed -i '/time.cloudflare.com/d; /time.google.com/d' /etc/hosts
+# 1. NTP Fix (Запись в hosts для старта при "холодном" DNS)
+sed -i '/time.cloudflare.com/d; /time.google.com/d; /ntp.yandex.ru/d' /etc/hosts
 echo "162.159.200.1 time.cloudflare.com" >> /etc/hosts
 echo "216.239.35.0 time.google.com" >> /etc/hosts
-echo "[1/4] NTP IP добавлены в hosts."
+echo "213.180.193.1 ntp.yandex.ru" >> /etc/hosts
+echo "[1/4] IP адреса NTP добавлены в /etc/hosts для надежности."
 
-# 2. Установка (с возможностью пропуска)
+# Настройка системного NTP через UCI (для РФ)
+echo "Конфигурация NTP серверов..."
+uci del system.ntp.server
+uci add_list system.ntp.server='ntp.yandex.ru'
+uci add_list system.ntp.server='0.ru.pool.ntp.org'
+uci add_list system.ntp.server='1.ru.pool.ntp.org'
+uci add_list system.ntp.server='ntp1.vniiftri.ru' # Эталонное время РФ (НИИР)
+uci add_list system.ntp.server='time.cloudflare.com'
+uci set system.ntp.enabled='1'
+uci set system.ntp.enable_server='0'
+uci commit system
+/etc/init.d/sysntpd restart
+
+# 2. Установка Unbound
 DO_OPKG=1
-echo "[?] Установить Unbound? Нажмите ЛЮБУЮ клавишу за 10 сек для ОТМЕНЫ."
-if read -t 10 -n 1; then
+echo "[?] Установить Unbound? Нажмите ЛЮБУЮ клавишу за 5 сек для ОТМЕНЫ."
+if read -t 5 -n 1; then
     echo -e "\n[!] Шаг установки пропущен."
     DO_OPKG=0
 else
@@ -26,18 +40,23 @@ mkdir -p /var/lib/unbound
 [ ! -f /var/lib/unbound/root.key ] && unbound-anchor -a /var/lib/unbound/root.key
 chown -R unbound:unbound /var/lib/unbound
 
-uci del unbound.@unbound[0].unbound_conf 2>/dev/null
+# Сброс и чистая настройка UCI
+uci del unbound.@unbound[0] 2>/dev/null
+uci add unbound unbound
 uci set unbound.@unbound[0].validator='1'
 uci set unbound.@unbound[0].port='5353'
+uci set unbound.@unbound[0].localservice='1'
 uci add_list unbound.@unbound[0].unbound_conf='auto-trust-anchor-file: "/var/lib/unbound/root.key"'
-uci add_list unbound.@unbound[0].unbound_conf='ntp-dot-allow: yes'
 uci commit unbound
 
 # 4. Привязка к Dnsmasq
 echo "[4/4] Перенаправление Dnsmasq -> Unbound (5353)..."
-while uci get dhcp.@dnsmasq[0].server >/dev/null 2>&1; do
-    uci del_list dhcp.@dnsmasq[0].server=$(uci get dhcp.@dnsmasq[0].server | awk '{print $1}')
+
+# Удаляем все старые серверы из dnsmasq
+while uci get dhcp.@dnsmasq[0].server | grep -q "."; do
+    uci del_list dhcp.@dnsmasq[0].server=$(uci get dhcp.@dnsmasq[0].server | head -n 1)
 done
+
 uci add_list dhcp.@dnsmasq[0].server='127.0.0.1#5353'
 uci set dhcp.@dnsmasq[0].noresolv='1'
 uci set dhcp.@dnsmasq[0].cachesize='1000'
@@ -45,4 +64,4 @@ uci commit dhcp
 
 /etc/init.d/unbound restart
 /etc/init.d/dnsmasq restart
-echo "=== ГОТОВО! ==="
+echo "=== ГОТОВО! Проверьте время командой 'date' ==="
